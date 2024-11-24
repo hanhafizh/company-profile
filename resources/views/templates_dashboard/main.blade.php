a9397b6c62cc805d24587b558e627a11752b5af4
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-11-24 17:55:15 +0700

    update-style-sidebar

--- resources/views/templates_dashboard/main.blade.php
+++ resources/views/templates_dashboard/main.blade.php
@@ -2,12 +2,17 @@
 <html lang="en">
     @include('templates_dashboard.head')
     <body>
-        <div class="d-flex">
+        <div class="d-flex flex-column flex-lg-row">
             <!-- Sidebar -->
             @include('templates_dashboard.sidebar')
 
             <!-- Main Content -->
             <div class="content flex-grow-1">
+                <!-- Toggle Sidebar Button for Mobile -->
+                <button class="btn btn-primary d-lg-none m-3" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+                    <i class="fa fa-bars"></i>
+                </button>
+                
                 <main class="p-4">
                     @yield('content')
                 </main>
--- resources/views/templates_dashboard/sidebar.blade.php
+++ resources/views/templates_dashboard/sidebar.blade.php
@@ -1,39 +1,32 @@
-<div class="sidebar bg-dark text-white vh-100 p-3" style="width: 250px;">
+<nav id="sidebar" class="sidebar bg-dark text-white vh-100 p-3 d-lg-block collapse">
     <h5 class="text-center mb-4">Dashboard</h5>
     <ul class="nav flex-column">
-        <!-- Dashboard Menu -->
         <li class="nav-item">
             <a href="{{ route('dashboard') }}" class="nav-link text-white {{ Request::routeIs('dashboard') ? 'active' : '' }}">
                 <i class="fa fa-home me-2"></i> Dashboard
             </a>
         </li>
-
-        <!-- Configuration Menu with Submenu -->
         <li class="nav-item">
-            <a class="nav-link text-white dropdown-toggle" data-bs-toggle="collapse" href="#configurationSubmenu" role="button" aria-expanded="false" aria-controls="configurationSubmenu">
+            <a href="#configSubmenu" data-bs-toggle="collapse" class="nav-link text-white">
                 <i class="fa fa-cogs me-2"></i> Configuration
             </a>
-            <div class="collapse ps-3" id="configurationSubmenu">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a href="{{ route('configuration.organization') }}" class="nav-link text-white {{ Request::routeIs('configuration.organization') ? 'active' : '' }}">
-                            <i class="fa fa-users me-2"></i> Organization
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('configuration.event') }}" class="nav-link text-white {{ Request::routeIs('configuration.event') ? 'active' : '' }}">
-                            <i class="fa fa-calendar-alt me-2"></i> Event
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            <ul id="configSubmenu" class="collapse list-unstyled ps-3">
+                <li>
+                    <a href="{{ route('configuration.organization') }}" class="nav-link text-white {{ Request::routeIs('configuration.organization') ? 'active' : '' }}">
+                        <i class="fa fa-users me-2"></i> Organization
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('configuration.event') }}" class="nav-link text-white {{ Request::routeIs('configuration.event') ? 'active' : '' }}">
+                        <i class="fa fa-calendar me-2"></i> Event
+                    </a>
+                </li>
+            </ul>
         </li>
-
-        <!-- Logout Menu -->
         <li class="nav-item">
             <a href="{{ route('logout') }}" class="nav-link text-white">
                 <i class="fa fa-sign-out-alt me-2"></i> Logout
             </a>
         </li>
     </ul>
-</div>
+</nav>
