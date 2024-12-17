892bb48ae37d14d398e0d8017ebaaae083bdb23b
hanhafizh <farhanhafizh770@gmail.com>
2024-12-17 20:29:43 +0700

    dinamis ourworks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ourworks;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,7 +24,10 @@ class HomeController extends Controller
 
     public function ourwork()
     {
-        return view('home.ourwork');
+        $ourworks = Ourworks::all();
+        return view('home.ourwork', compact(
+            'ourworks',
+        ));
     }
 
     public function event()
--- resources/views/home/ourwork.blade.php
+++ resources/views/home/ourwork.blade.php
@@ -13,61 +13,29 @@
                 </p>
             </div>
             <div class="row g-4 justify-content-center">
-                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="blog-item">
-                        <div class="blog-img">
-                            <img src="img/blog-1.png" class="img-fluid w-100" alt="">
-                            {{-- <div class="blog-info">
+                @foreach ($ourworks as $ourwork)
+                    <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="blog-item">
+                            <div class="blog-img">
+                                <img src="/image/ourworks/{{ $ourwork->image }}" class="img-fluid w-100" alt="">
+                                {{-- <div class="blog-info">
                                 <span><i class="fa fa-clock"></i> Dec 01.2024</span>
                                 <div class="d-flex">
                                     <span class="me-3"> 3 <i class="fa fa-heart"></i></span>
                                     <a href="#" class="text-white">0 <i class="fa fa-comment"></i></a>
                                 </div>
                             </div> --}}
-                        </div>
-                        <div class="blog-content text-dark border p-4 ">
-                            <h5 class="mb-4">Dolor, sit amet consectetur adipisicing</h5>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectur adip sed eiusmod amet consectur adip.</p>
-                            <a class="btn btn-light rounded-pill py-2 px-4" href="#">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="blog-item">
-                        <div class="blog-img">
-                            <img src="img/blog-2.png" class="img-fluid w-100" alt="">
-                        </div>
-                        <div class="blog-content text-dark border p-4 ">
-                            <h5 class="mb-4">Dolor, sit amet consectetur adipisicing</h5>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectur adip sed eiusmod amet consectur adip.</p>
-                            <a class="btn btn-light rounded-pill py-2 px-4" href="#">Read More</a>
+                            </div>
+                            <div class="blog-content text-dark border p-4 ">
+                                <h5 class="mb-4">{{ $ourwork->title }}</h5>
+                                <p class="mb-4">{{ \Illuminate\Support\Str::limit($ourwork->description, 100, '...') }}
+                                </p>
+
+                                <a class="btn btn-light rounded-pill py-2 px-4" href="#">Read More</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="blog-item">
-                        <div class="blog-img">
-                            <img src="img/blog-3.png" class="img-fluid w-100" alt="">
-                        </div>
-                        <div class="blog-content text-dark border p-4 ">
-                            <h5 class="mb-4">Dolor, sit amet consectetur adipisicing</h5>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectur adip sed eiusmod amet consectur adip.</p>
-                            <a class="btn btn-light rounded-pill py-2 px-4" href="#">Read More</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="blog-item">
-                        <div class="blog-img">
-                            <img src="img/blog-4.png" class="img-fluid w-100" alt="">
-                        </div>
-                        <div class="blog-content text-dark border p-4 ">
-                            <h5 class="mb-4">Dolor, sit amet consectetur adipisicing</h5>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectur adip sed eiusmod amet consectur adip.</p>
-                            <a class="btn btn-light rounded-pill py-2 px-4" href="#">Read More</a>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
