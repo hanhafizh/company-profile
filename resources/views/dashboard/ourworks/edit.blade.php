590cf3f0e724982f8784b98919460052cea9289e
hanhafizh <farhanhafizh770@gmail.com>
2024-12-17 17:40:50 +0700

    update edit in crud our works

--- resources/views/dashboard/ourworks/edit.blade.php
+++ resources/views/dashboard/ourworks/edit.blade.php
@@ -1,54 +1,67 @@
 @extends('templates_dashboard.main')
 
-@section('title', 'Our Works')
+@section('title', 'Edit Our Work')
 
 @section('content')
 
-    <div class="container">
+    <div class="content-header">
         <div class="container-fluid">
-
-            <a href="/ourworks" class="btn btn-primary mb-3">Kembali</a>
-            <div class="row">
-                <div class="col-md-12">
-                    <form action="{{ route('ourworks.update', $ourwork->id) }}" method="POST" enctype="multipart/form-data">
-                        @method('PUT')
-                        @csrf
-                        <div class="form-group">
-                            <label for="">Title</label>
-                            <input type="text" class="form-control" name="title" placeholder="Title"
-                                value="{{ $ourwork->title }}"> <!-- Ganti dari $ourworks menjadi $ourwork -->
-                        </div>
-                        @error('title')
-                            <small style="color:red">{{ $message }}</small>
-                        @enderror
-                        <div class="form-group">
-                            <label for="">Description</label>
-                            <textarea name="description" id="" cols="30" rows="10" class="form-control"
-                                placeholder="Description">{{ $ourwork->description }}</textarea> <!-- Ganti dari $ourworks menjadi $ourwork -->
-                        </div>
-                        @error('description')
-                            <small style="color:red">{{ $message }}</small>
-                        @enderror
-                        <img src="/image/ourworks/{{ $ourwork->image }}" alt="" class="img-fluid">
-                        <!-- Ganti dari $ourworks menjadi $ourwork -->
-                        <div class="form-group">
-                            <label for="">Image</label>
-                            <input type="file" class="form-control" name="image">
-                        </div>
-                        @error('image')
-                            <small style="color:red">{{ $message }}</small>
-                        @enderror
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-block">Submit</button>
-                        </div>
-                    </form>
-
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Edit Our Work</h1>
                 </div>
             </div>
         </div>
-
-    </div>
     </div>
 
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title mb-0">
+                                Edit Our Work</h5>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('ourworks.update', $ourwork->id) }}" method="POST"
+                                enctype="multipart/form-data">
+                                @method('PUT')
+                                @csrf
+                                <div class="form-group">
+                                    <label for="">Title</label>
+                                    <input type="text" class="form-control" name="title" placeholder="Title"
+                                        value="{{ $ourwork->title }}">
+                                </div>
+                                @error('title')
+                                    <small style="color:red">{{ $message }}</small>
+                                @enderror
+                                <div class="form-group">
+                                    <label for="">Description</label>
+                                    <textarea name="description" id="" cols="30" rows="10" class="form-control"
+                                        placeholder="Description">{{ $ourwork->description }}</textarea>
+                                </div>
+                                @error('description')
+                                    <small style="color:red">{{ $message }}</small>
+                                @enderror
+                                <img src="/image/ourworks/{{ $ourwork->image }}" alt="" class="img-fluid">
+                                <div class="form-group">
+                                    <label for="">Image</label>
+                                    <input type="file" class="form-control" name="image">
+                                </div>
+                                @error('image')
+                                    <small style="color:red">{{ $message }}</small>
+                                @enderror
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
