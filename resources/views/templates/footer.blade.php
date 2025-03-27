95bb0bdd008ae3d2c9a70bf9933860cb348d8e89
hanhafizh <farhanhafizh770@gmail.com>
2025-03-27 14:29:55 +0700

    footer

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -4,12 +4,12 @@
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="text-dark mb-4">Company</h4>
-                    <a href="">Why Mailler?</a>
+                    {{-- <a href="">Why Mailler?</a> --}}
                     <a href="">Our Features</a>
                     <a href="">Our Portfolio</a>
                     <a href="">About Us</a>
-                    <a href="">Our Blog & News</a>
-                    <a href="">Get In Touch</a>
+                    {{-- <a href="">Our Blog & News</a>
+                    <a href="">Get In Touch</a> --}}
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
@@ -18,9 +18,9 @@
                     <a href="">About Us</a>
                     <a href="">Contact Us</a>
                     <a href="">Privacy Policy</a>
-                    <a href="">Terms & Conditions</a>
+                    {{-- <a href="">Terms & Conditions</a>
                     <a href="">Our Blog & News</a>
-                    <a href="">Our Team</a>
+                    <a href="">Our Team</a> --}}
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
@@ -29,24 +29,28 @@
                     <a href="">All Services</a>
                     <a href="">Promotional Emails</a>
                     <a href="">Product Updates</a>
-                    <a href="">Email Marketing</a>
+                    {{-- <a href="">Email Marketing</a>
                     <a href="">Acquistion Emails</a>
-                    <a href="">Retention Emails</a>
+                    <a href="">Retention Emails</a> --}}
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-dark">Contact Info</h4>
-                    <a href=""><i class="fa fa-map-marker-alt me-2"></i>123 Street, New York, USA</a>
+                    <a href=""><i class="fa fa-map-marker-alt me-2"></i>123 Street, Jakarta, Indonesia</a>
                     <a href=""><i class="fas fa-envelope me-2"></i>info@example.com</a>
                     <a href=""><i class="fas fa-phone me-2"></i>+012 345 67890</a>
                     <a href=""><i class="fas fa-print me-2"></i>+012 345 67890</a>
                     <div class="d-flex align-items-center">
                         <i class="fas fa-share fa-2x text-secondary me-2"></i>
-                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-instagram"></i></a>
-                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
+                                class="fab fa-twitter"></i></a>
+                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
+                                class="fab fa-instagram"></i></a>
+                        <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
+                                class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
@@ -60,10 +64,12 @@
     <div class="container">
         <div class="row g-4 align-items-center">
             <div class="col-md-6 text-center text-md-start mb-md-0">
-                <span class="text-white"><a href="#"><i class="fas fa-copyright text-light me-2"></i>PT. Karya Prima Usahatama</a>, All right reserved.</span>
+                <span class="text-white"><a href="#"><i class="fas fa-copyright text-light me-2"></i>PT. Karya
+                        Prima Usahatama</a>, All right reserved.</span>
             </div>
             <div class="col-md-6 text-center text-md-end text-white">
-                Designed By <a target="_blank" class="border-bottom" href="https://kikiaka.id">KIKIAKA DIGITAL PROJECT</a>
+                Designed By <a target="_blank" class="border-bottom" href="https://kikiaka.id">KIKIAKA DIGITAL
+                    PROJECT</a>
             </div>
         </div>
     </div>
