8206e8cdcabff51a2797a978258d89f9f6a27f01
hanhafizh <farhanhafizh770@gmail.com>
2025-04-07 13:39:16 +0700

    servicelist: new seeders and crud

--- resources/views/components/view/service/service.blade.php
+++ resources/views/components/view/service/service.blade.php
@@ -15,8 +15,8 @@
                  <div class="col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch wow fadeInUp" data-wow-delay="0.1s">
                      <div class="service-item text-center rounded p-4 w-100">
                          <div class="service-icon d-inline-block bg-light rounded p-4 mb-4">
-                             <img src="/image/servicelist/{{ $list->image }}" alt="Image"
-                                 class="img-fluid rounded-circle" style="width: 90px; height: 90px; object-fit: cover;">
+                             <img src="/image/servicelist/{{ $list->image }}" alt="Image" class="img-fluid"
+                                 style="width: 90px; height: 90px; object-fit: cover;">
                          </div>
 
                          <div class="service-content">
