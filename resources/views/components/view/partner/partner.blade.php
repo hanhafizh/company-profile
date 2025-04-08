ee1013db8f06599eba160d1b7ae301b59556227e
hanhafizh <farhanhafizh770@gmail.com>
2025-04-08 13:33:42 +0700

    ourpartner: blade

---
+++ resources/views/components/view/partner/partner.blade.php
@@ -0,0 +1,40 @@
+   <!-- Partner Start -->
+   <div class="container mt-5">
+       <section class="py-3 py-md-5 py-xl-8">
+           <div class="container">
+               <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 900px;">
+                   <h4 class="text-primary">Our Clients</h4>
+                   <h1 class="display-5 mb-4">Trusted by over 786+ clients.</h1>
+                   <p class="mb-0">Our clients are our top priority, and we are committed to providing them with the
+                       highest level of service.
+                   </p>
+               </div>
+           </div>
+
+           <div class="container overflow-hidden">
+               <div class="row gy-4">
+                   <div class="col-6 col-md-4 col-xl-3 text-center">
+                       <div class="text-secondary bg-light px-4 py-3 px-md-6 py-md-4 px-lg-8 py-lg-5">
+                           <img src="/image/featurelist/feature1.png" class="img-fluid w-50 h-50" alt="feature1">
+                       </div>
+                   </div>
+                   <div class="col-6 col-md-4 col-xl-3 text-center">
+                       <div class="text-secondary bg-light px-4 py-3 px-md-6 py-md-4 px-lg-8 py-lg-5">
+                           <img src="/image/featurelist/feature2.png" class="img-fluid w-50 h-50" alt="feature2">
+                       </div>
+                   </div>
+                   <div class="col-6 col-md-4 col-xl-3 text-center">
+                       <div class="text-secondary bg-light px-4 py-3 px-md-6 py-md-4 px-lg-8 py-lg-5">
+                           <img src="/image/featurelist/feature3.png" class="img-fluid w-50 h-50" alt="feature3">
+                       </div>
+                   </div>
+                   <div class="col-6 col-md-4 col-xl-3 text-center">
+                       <div class="text-secondary bg-light px-4 py-3 px-md-6 py-md-4 px-lg-8 py-lg-5">
+                           <img src="/image/featurelist/feature4.png" class="img-fluid w-50 h-50" alt="feature4">
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </section>
+   </div>
+   <!-- Partner End -->
