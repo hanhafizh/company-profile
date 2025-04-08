18556bfb628c724a7c2f3f11bd5a0bd12968aa63
hanhafizh <farhanhafizh770@gmail.com>
2025-04-08 15:09:29 +0700

    footers: updates

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -4,20 +4,18 @@
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="text-dark mb-4">Company</h4>
-                    {{-- <a href="">Why Mailler?</a> --}}
-                    <a href="">Our Features</a>
-                    <a href="">Our Portfolio</a>
-                    <a href="">About Us</a>
-                    {{-- <a href="">Our Blog & News</a>
-                    <a href="">Get In Touch</a> --}}
+                    <a href="{{ route('event') }}">Our Events</a>
+                    <a href="{{ route('partner') }}">Our Partner</a>
+                    {{-- <a href="{{ route('contact') }}">Our Contact</a> --}}
+                    {{-- <a href="">About Us</a> --}}
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-dark">Quick Links</h4>
-                    <a href="">About Us</a>
-                    <a href="">Contact Us</a>
-                    <a href="">Privacy Policy</a>
+                    <a href="{{ route('about') }}">About Us</a>
+                    <a href="{{ route('contact') }}">Contact Us</a>
+                    {{-- <a href="">Privacy Policy</a> --}}
                     {{-- <a href="">Terms & Conditions</a>
                     <a href="">Our Blog & News</a>
                     <a href="">Our Team</a> --}}
@@ -26,9 +24,8 @@
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-dark">Services</h4>
-                    <a href="">All Services</a>
-                    <a href="">Promotional Emails</a>
-                    <a href="">Product Updates</a>
+                    <a href="{{ route('services') }}">All Services</a>
+
                     {{-- <a href="">Email Marketing</a>
                     <a href="">Acquistion Emails</a>
                     <a href="">Retention Emails</a> --}}
@@ -37,11 +34,10 @@
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-dark">Contact Info</h4>
-                    <a href=""><i class="fa fa-map-marker-alt me-2"></i>123 Street, Jakarta, Indonesia</a>
-                    <a href=""><i class="fas fa-envelope me-2"></i>info@example.com</a>
-                    <a href=""><i class="fas fa-phone me-2"></i>+012 345 67890</a>
-                    <a href=""><i class="fas fa-print me-2"></i>+012 345 67890</a>
-                    <div class="d-flex align-items-center">
+                    <a href=""><i class="fa fa-map-marker-alt me-2"></i>Jakarta Barat, 11140.</a>
+                    <a href=""><i class="fas fa-envelope me-2"></i>kpu@kaprima.com</a>
+                    <a href=""><i class="fas fa-phone me-2"></i>(021) 6343558</a>
+                    {{-- <div class="d-flex align-items-center">
                         <i class="fas fa-share fa-2x text-secondary me-2"></i>
                         <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
                                 class="fab fa-facebook-f"></i></a>
@@ -51,7 +47,7 @@
                                 class="fab fa-instagram"></i></a>
                         <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
                                 class="fab fa-linkedin-in"></i></a>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
