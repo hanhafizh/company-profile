b32225d8461db589b1b666c73aecf63161b511c2
hanhafizh <farhanhafizh770@gmail.com>
2025-04-10 11:08:10 +0700

    sidebar: change to red

--- resources/views/templates_dashboard/sidebar.blade.php
+++ resources/views/templates_dashboard/sidebar.blade.php
@@ -1,4 +1,5 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-danger elevation-4">
+
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
         <span class="brand-text font-weight-light" style="font-size: 20px"><strong>Karya Prima Usahatama</strong></span>
