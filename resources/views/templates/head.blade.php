360d6d1ba8c0dc775033dd68aefff8fde63e2946
hanhafizh <farhanhafizh770@gmail.com>
2025-04-18 16:06:33 +0700

    update favico $ description

--- resources/views/templates/head.blade.php
+++ resources/views/templates/head.blade.php
@@ -1,17 +1,20 @@
 <head>
     <meta charset="utf-8">
     <title>Karya Prima Usahatama</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Karya Prima Usahatama, layanan, perusahaan, jasa">
+    <meta name="description"
+        content="Karya Prima Usahatama adalah perusahaan yang menyediakan layanan profesional di berbagai bidang.">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;600;700&family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;600;700&family=Rubik:wght@400;500&display=swap"
+        rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
