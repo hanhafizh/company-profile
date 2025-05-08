cde9682773fa920299108da7fd865d1c7bf81fe3
hanhafizh <farhanhafizh770@gmail.com>
2025-05-08 15:50:46 +0700

    fix data: footers

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -6,8 +6,7 @@
                     <h4 class="text-dark mb-4">Company</h4>
                     <a href="{{ route('event') }}">Our Events</a>
                     <a href="{{ route('partner') }}">Our Partner</a>
-                    {{-- <a href="{{ route('contact') }}">Our Contact</a> --}}
-                    {{-- <a href="">About Us</a> --}}
+
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
@@ -15,28 +14,27 @@
                     <h4 class="mb-4 text-dark">Quick Links</h4>
                     <a href="{{ route('about') }}">About Us</a>
                     <a href="{{ route('contact') }}">Contact Us</a>
-                    {{-- <a href="">Privacy Policy</a> --}}
-                    {{-- <a href="">Terms & Conditions</a>
-                    <a href="">Our Blog & News</a>
-                    <a href="">Our Team</a> --}}
                 </div>
             </div>
-            <div class="col-md-6 col-lg-6 col-xl-3">
+            <div class="col-md-6 col-lg-6 col-xl-2">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-dark">Services</h4>
                     <a href="{{ route('services') }}">All Services</a>
-
-                    {{-- <a href="">Email Marketing</a>
-                    <a href="">Acquistion Emails</a>
-                    <a href="">Retention Emails</a> --}}
                 </div>
             </div>
-            <div class="col-md-6 col-lg-6 col-xl-3">
+            <div class="col-md-6 col-lg-6 col-xl-4">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-dark">Contact Info</h4>
-                    <a href=""><i class="fa fa-map-marker-alt me-2"></i>Jakarta Barat, 11140.</a>
-                    <a href=""><i class="fas fa-envelope me-2"></i>kpu@kaprima.com</a>
-                    <a href=""><i class="fas fa-phone me-2"></i>(021) 6343558</a>
+                    <a href="https://maps.app.goo.gl/MqavjGokCBZv5ixF9" target="_blank">
+                        <i class="fa fa-map-marker-alt me-2"></i>Ruko Ketapang Indah A2/8 Jakarta Barat 11140
+                    </a>
+                    <a href="mailto:contact@pt-kpusahatama.com">
+                        <i class="fas fa-envelope me-2"></i>contact@pt-kpusahatama.com
+                    </a>
+                    <a href="tel:+62216343558">
+                        <i class="fas fa-phone me-2"></i>(021) 6343558
+                    </a>
+
                     {{-- <div class="d-flex align-items-center">
                         <i class="fas fa-share fa-2x text-secondary me-2"></i>
                         <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i
