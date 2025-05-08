701bec73ae889d39d290829183da5f27cd51277d
hanhafizh <farhanhafizh770@gmail.com>
2025-05-08 15:37:25 +0700

    remove: designed by jendela kode

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -59,13 +59,13 @@
 <div class="container-fluid copyright py-4">
     <div class="container">
         <div class="row g-4 align-items-center">
-            <div class="col-md-6 text-center text-md-start mb-md-0">
+            <div class="col-md-12 text-center text-md-center mb-md-0">
                 <span class="text-white"><a href="#"><i class="fas fa-copyright text-light me-2"></i>PT. Karya
                         Prima Usahatama</a>, All right reserved.</span>
             </div>
-            <div class="col-md-6 text-center text-md-end text-white">
+            {{-- <div class="col-md-6 text-center text-md-end text-white">
                 Designed by <a href="#">Jendela Kode</a>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
